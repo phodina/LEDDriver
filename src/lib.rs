fcd17c069850aacaa84ba05b5056931191ab4872
Petr Hodina <hodinapetr46@gmail.com>
2017-11-20 22:59:03 +0100

    Simple platform agnostic LED driver written in Rust using TDD. Code will be based on C code from Test Driven Development in C. Specified list of requirements.

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tests {
+
+    // TODO: All LEDs shall be off after the driver initialization.
+    // TODO: A single LED can be turned on.
+    // TODO: A single LED can be turned off.
+    // TODO: Multiple LEDs can be turned on/off.
+    // TODO: Turn on all LEDs.
+    // TODO: Turn off all LEDs.
+    // TODO: Query LED state.
+    // TODO: Check boundary values.
+    // TODO: Check out-of bounds values.
+}
