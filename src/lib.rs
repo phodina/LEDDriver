994965ce22c98b155476c2024ce7d619c1e54e97
Petr Hodina <hodinapetr46@gmail.com>
2017-11-20 23:24:04 +0100

    Created test for REQ:"leds off after initialization".

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
+struct LEDDriver<'a>{
+	address : &'a u32
+}
+
+impl <'a>LEDDriver<'a> {
+
+	fn new(address : &'a u32) -> Self {
+		LEDDriver { address : address}
+	}
+}
+
 #[cfg(test)]
 mod tests {
 
+	use super::*;
     // TODO: All LEDs shall be off after the driver initialization.
+    #[test]
+    fn driver_init_leds_off () {
+    	let addr = &0xffffffff;
+    	let leddriver = LEDDriver::new(addr);
+    	assert_eq!(0, *addr);
+    }
     // TODO: A single LED can be turned on.
     // TODO: A single LED can be turned off.
     // TODO: Multiple LEDs can be turned on/off.
