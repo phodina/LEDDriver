dfa0b81db9fbe4dc845d8e5b25fadbafcb3afd05
Petr Hodina <hodinapetr46@gmail.com>
2017-11-21 15:41:22 +0100

    Implemented test for REQ:"A single LED can be turned on".

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
 use std::cell::Cell;
 
+enum LEDs {
+    Led1,
+    Led2,
+    Led3,
+    Led4,
+    Led5,
+    Led6,
+    Led7,
+    Led8,
+    Led9,
+    Led10,
+    Led11,
+    Led12,
+    Led13,
+    Led14,
+    Led15,
+    Led16,
+}
+
 struct LEDDriver<'a>{
 	address: &'a Cell<u32>
 }
@@ -12,6 +31,27 @@ impl <'a>LEDDriver<'a> {
 		leddriver.address.set(0);
 		leddriver
 	}
+
+    fn turn_on (&mut self, led : LEDs) {
+        match led {
+            LEDs::Led1 => self.address.set(1<<0),
+            LEDs::Led2 => self.address.set(1<<1),
+            LEDs::Led3 => self.address.set(1<<2),
+            LEDs::Led4 => self.address.set(1<<3),
+            LEDs::Led5 => self.address.set(1<<4),
+            LEDs::Led6 => self.address.set(1<<5),
+            LEDs::Led7 => self.address.set(1<<6),
+            LEDs::Led8 => self.address.set(1<<7),
+            LEDs::Led9 => self.address.set(1<<8),
+            LEDs::Led10 => self.address.set(1<<9),
+            LEDs::Led11 => self.address.set(1<<10),
+            LEDs::Led12 => self.address.set(1<<11),
+            LEDs::Led13 => self.address.set(1<<12),
+            LEDs::Led14 => self.address.set(1<<13),
+            LEDs::Led15 => self.address.set(1<<14),
+            LEDs::Led16 => self.address.set(1<<15),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -28,6 +68,15 @@ mod tests {
     	assert_eq!(0, addr.get());
     }
     // TODO: A single LED can be turned on.
+    #[test]
+    fn diver_single_led_on () {
+
+        let ref mut addr = Cell::new(0x00000000);
+        let mut leddriver = LEDDriver::new(addr);
+        leddriver.turn_on(LEDs::Led1);
+        assert_eq!(1, addr.get());
+
+    }
     // TODO: A single LED can be turned off.
     // TODO: Multiple LEDs can be turned on/off.
     // TODO: Turn on all LEDs.
